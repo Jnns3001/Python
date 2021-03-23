d7d564492b0fa01039867f9a2f91f565f055756a
Jnns3001 <jannismayer3001@gmail.com>
2021-03-23 14:59:38 +0100

    Added ShutDown.py

---
+++ shutDown.py
@@ -0,0 +1,20 @@
+import os
+import time
+from datetime import datetime
+
+
+
+def doShutDown():
+    print("done")
+    os.system("shutdown /s /t 1")
+
+while True:
+    allnames = ""
+    for file in os.listdir("C:\\Users\\Jannis\\Downloads\\"):
+        allnames += file
+    if allnames.find(".part") < 0:
+        doShutDown()
+    else:
+        print("still Downloading    " + str(datetime.now(tz=None)))
+    time.sleep(60)       
+
