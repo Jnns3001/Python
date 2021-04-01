5b74afa5b585ac80298983e28ed34c92613f7d02
Jnns3001 <jannismayer3001@gmail.com>
2021-04-01 15:42:50 +0200

    Improved shutDown

--- shutDown.py
+++ shutDown.py
@@ -7,7 +7,7 @@ from datetime import datetime
 def doShutDown():
     print("done")
     os.system("shutdown /s /t 1")
-
+    exit()
 while True:
     allnames = ""
     for file in os.listdir("C:\\Users\\Jannis\\Downloads\\"):
@@ -16,5 +16,5 @@ while True:
         doShutDown()
     else:
         print("still Downloading    " + str(datetime.now(tz=None)))
-    time.sleep(60)       
+    time.sleep(10)       
 
