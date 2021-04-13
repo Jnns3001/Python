26adfc9e4f3eb43542b6de00e0d2eda49ed4269c
Jnns3001 <jannismayer3001@gmail.com>
2021-04-13 07:52:07 +0200

    Added snake

---
+++ snake/snake.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+
+#config
+tileSize = 16
+mapWidth, mapHeight = 50, 50
+
+floorTile = r'snake\img\floor'
+fruitTile = r'snake\img\fruit'
+snakeHeadTile = r'snake\img\snakeHead'
+snakeBodyTile = r'snake\img\snakeBody'
+snakeTailTile = r'snake\img\snakeTail'
