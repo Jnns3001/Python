4049586ae295ee00f9e339182f58264f232d30b9
Jnns3001 <jannismayer@protonmail.com>
2024-05-08 06:44:07 +0200

    refactor

--- Img/ImageAdder.py
+++ Img/ImageAdder.py
@@ -1,45 +1,97 @@
 import cv2
 import numpy as np
 
-directory = "../../HLL/PanzerTaschenBuch/Bilder/Skaliert/ISO/"
-outdir = directory + "out/"
+filetype = ".png"
+img_size = [2000, 1150]
 
 
-def create_gallery(img_names, name):
+def cg(name, img_names, axis=1):
     images = []
     for img_name in img_names:
-        images.append(cv2.imread(directory + img_name))
+        images.append(cv2.imread(directory + img_name + filetype))
+    concat = np.concatenate(images, axis=axis)
 
-    print(images)
-    vis = np.concatenate(images, axis=1)
-    cv2.imshow(name, vis)
-    cv2.imwrite(outdir + name, vis)
+    cv2.imwrite(directory + name + filetype, concat)
+
+"""
+directory = "C:/Users/Jannis/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/ISO/"
+cv2.imwrite(directory + "white.png",np.full((1125, 2000, 3), 255, "uint8"))
+cg("Germany", ['Tiger', 'Panther', 'P4', 'Luchs', 'Puma'])
+cg("America", ['j76', 'j75', 'm75', 'Stuart', 'Greyhound'])
+cg("Russia", ['is1', 't34', 't70', 'ba10'])
+cg("British", ['Churchill', 'Firefly', 'Crusader', 'Cromwell', 'Honey', 'Tetrarch', 'Daimler'])
+cg("AlliesRecon", ["Greyhound", "ba10", "Daimler", "white", "white"])
+cg("AlliesLight", ["Stuart", "t70", "Honey", "Tetrarch", "white"])
+cg("AlliesMedium", ["m75", "t34", "Crusader", "Cromwell", "white"])
+cg("AlliesHeavy", ["j76", "j75", "is1", "Churchill", "Firefly"])
+cg("Allies", ["AlliesRecon", "AlliesLight", "AlliesMedium", "AlliesHeavy"], axis=0)
+cg("Axis", ["Puma", "Luchs", "P4", "Panther", "Tiger"], axis=0)
+"""
+"""
+directory = "C:/Users/Jannis/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/HE/"
+cg("HELuchs", ["Luchs back", "Luchs side"], axis=0)
+cg("HEStuart", ["Stuart back", "Stuart side"], axis=0)
+cg("HEt70", ["t70 back", "t70 side"], axis=0)
+cg("HEHoney", ["Honey back", "Honey side"], axis=0)
+cg("HETetrarch", ["Tetrarch back", "Tetrarch side"], axis=0)
+cg("HE", ["HELuchs", "HEStuart", "HEt70", "HEHoney", "HETetrarch"])
+"""
 
 
-deutsche = ('Tiger.png', 'Panther.png', 'P4.png', 'P4.png', 'Puma.png')  # Luchs placeholder
-#create_gallery(deutsche, "Deutsche")
+directory = "C:/Users/Jannis/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/Motor/"
+"""
+cv2.imwrite(directory + "white.png", np.full((1800, 1200, 3), 255, "uint8"))
+cg("EngineLuchs", ["Luchs back", "Luchs side"], axis=0)
+cg("EngineStuart", ["Stuart back", "Stuart side"], axis=0)
+cg("Enginet70", ["t70 back", "t70 side"], axis=0)
+cg("EngineHoney", ["Honey back", "Honey side"], axis=0)
+cg("EngineTetrarch", ["Tetrarch back", "Tetrarch side"], axis=0)
 
-img1 = cv2.imread(directory + 'j76.png')
-img2 = cv2.imread(directory + 'j75.png')
-img3 = cv2.imread(directory + 'm75.png')
-img4 = cv2.imread(directory + 'Stuart.png')
-img5 = cv2.imread(directory + 'Greyhound.png')
-vis = np.concatenate([img1, img2, img3, img4, img5], axis=1)
-cv2.imwrite(directory + 'out/Amerikaner.png', vis)
+cg("EngineP4", ["P4 back", "P4 side"], axis=0)
+cg("Enginem75", ["m75 back", "m75 side"], axis=0)
+cg("Enginet34", ["t34 back", "t34 side"], axis=0)
+cg("EngineCrusader", ["Crusader back", "Crusader side"], axis=0)
+cg("EngineCromwell", ["Cromwell back", "Cromwell side"], axis=0)
 
-img1 = cv2.imread(directory + 'is1.png')
-img2 = cv2.imread(directory + 't34.png')
-img3 = cv2.imread(directory + 't70.png')
-img4 = cv2.imread(directory + 'ba10.png')
-vis = np.concatenate((img1, img2, img3, img4), axis=1)
-cv2.imwrite(directory + 'out/Russen.png', vis)
+cg("EngineTiger", ["Tiger back", "Tiger side"], axis=0)
+cg("EnginemPanther", ["Panther back", "Panther side"], axis=0)
+cg("Enginetj76", ["j76 back", "j76 side"], axis=0)
+cg("Engineis1", ["is1 back", "is1 side"], axis=0)
+cg("EngineChurchill", ["Churchill back", "Churchill side"], axis=0)
+cg("EngineFirefly", ["Firefly back", "Firefly side"], axis=0)
 
-img1 = cv2.imread(directory + 'Churchill.png')
-img2 = cv2.imread(directory + 'Firefly.png')
-img3 = cv2.imread(directory + 'Crusader.png')
-img4 = cv2.imread(directory + 'Cromwell.png')
-img5 = cv2.imread(directory + 'Honey.png')
-img6 = cv2.imread(directory + 'Tetrarch.png')
-img7 = cv2.imread(directory + 'Daimler.png')
-vis = np.concatenate((img1, img2, img3, img4, img5, img6, img7), axis=1)
-cv2.imwrite(directory + 'out/Briten.png', vis)
+cg("EngineGerman", ["EngineLuchs", "EngineP4", "EngineTiger", "EnginemPanther", "white", "white"])
+cg("EngineAmerican", ["EngineStuart", "Enginem75", "Enginetj76", "white", "white", "white"])
+cg("EngineRussian", ["Enginet70", "Enginet34", "Engineis1", "white", "white", "white"])
+cg("EngineBritish", ["EngineHoney", "EngineTetrarch", "EngineCrusader", "EngineCromwell", "EngineChurchill", "EngineFirefly"])
+"""
+
+
+cg("EngineLuchs", ["Luchs back", "Luchs side"], axis=0)
+cg("EngineStuart", ["Stuart back", "Stuart side"], axis=0)
+cg("Enginet70", ["t70 back", "t70 side"], axis=0)
+cg("EngineHoney", ["Honey back", "Honey side"], axis=0)
+cg("EngineTetrarch", ["Tetrarch back", "Tetrarch side"], axis=0)
+
+cg("EngineLight", ["EngineLuchs", "EngineStuart", "Enginet70", "EngineHoney", "EngineTetrarch", "white"])
+
+cg("EngineP4", ["P4 back", "P4 side"], axis=0)
+cg("Enginem75", ["m75 back", "m75 side"], axis=0)
+cg("Enginet34", ["t34 back", "t34 side"], axis=0)
+cg("EngineCrusader", ["Crusader back", "Crusader side"], axis=0)
+cg("EngineCromwell", ["Cromwell back", "Cromwell side"], axis=0)
+
+cg("EngineMedium", ["EngineP4", "Enginem75", "Enginet34", "EngineCrusader", "EngineCromwell", "white"])
+
+cg("EngineTiger", ["Tiger back", "Tiger side"], axis=0)
+cg("EnginemPanther", ["Panther back", "Panther side"], axis=0)
+cg("Enginetj76", ["j76 back", "j76 side"], axis=0)
+cg("Engineis1", ["is1 back", "is1 side"], axis=0)
+cg("EngineChurchill", ["Churchill back", "Churchill side"], axis=0)
+
+cg("EngineFirefly", ["Firefly back", "Firefly side"], axis=0)
+
+cg("EngineHeavy", ["EngineTiger", "EnginemPanther", "Enginetj76", "Engineis1", "EngineChurchill", "EngineFirefly"])
+
+cg("Engine", ["EngineLight", "EngineMedium", "EngineHeavy"], axis=0)
+
