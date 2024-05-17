f9dc30afb9af16cc58534b21a2e80c1188e0c6b1
Jnns3001 <78305903+Jnns3001@users.noreply.github.com>
2024-05-17 13:17:43 +0200

    work on image adder

--- Img/ImageAdder.py
+++ Img/ImageAdder.py
@@ -13,8 +13,10 @@ def cg(name, img_names, axis=1):
 
     cv2.imwrite(directory + name + filetype, concat)
 
-"""
-directory = "C:/Users/Jannis/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/ISO/"
+
+#"""
+directory = "C:/Users/Jannis Z30-C/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/ISO/"
+# directory = "C:/Users/Jannis/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/ISO/"
 cv2.imwrite(directory + "white.png",np.full((1125, 2000, 3), 255, "uint8"))
 cg("Germany", ['Tiger', 'Panther', 'P4', 'Luchs', 'Puma'])
 cg("America", ['j76', 'j75', 'm75', 'Stuart', 'Greyhound'])
@@ -28,43 +30,26 @@ cg("Allies", ["AlliesRecon", "AlliesLight", "AlliesMedium", "AlliesHeavy"], axis
 cg("Axis", ["Puma", "Luchs", "P4", "Panther", "Tiger"], axis=0)
 """
 """
-directory = "C:/Users/Jannis/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/HE/"
+directory = "C:/Users/Jannis Z30-C/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/HE/"
+# directory = "C:/Users/Jannis/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/HE/"
 cg("HELuchs", ["Luchs back", "Luchs side"], axis=0)
 cg("HEStuart", ["Stuart back", "Stuart side"], axis=0)
 cg("HEt70", ["t70 back", "t70 side"], axis=0)
 cg("HEHoney", ["Honey back", "Honey side"], axis=0)
 cg("HETetrarch", ["Tetrarch back", "Tetrarch side"], axis=0)
 cg("HE", ["HELuchs", "HEStuart", "HEt70", "HEHoney", "HETetrarch"])
-"""
+#"""
 
+directory = "C:/Users/Jannis Z30-C/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/Motor/"
+# directory = "C:/Users/Jannis/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/Motor/"
+#"""
 
-directory = "C:/Users/Jannis/Documents/HLL/PanzerTaschenBuch/Bilder/Skaliert/Motor/"
-"""
-cv2.imwrite(directory + "white.png", np.full((1800, 1200, 3), 255, "uint8"))
-cg("EngineLuchs", ["Luchs back", "Luchs side"], axis=0)
-cg("EngineStuart", ["Stuart back", "Stuart side"], axis=0)
-cg("Enginet70", ["t70 back", "t70 side"], axis=0)
-cg("EngineHoney", ["Honey back", "Honey side"], axis=0)
-cg("EngineTetrarch", ["Tetrarch back", "Tetrarch side"], axis=0)
-
-cg("EngineP4", ["P4 back", "P4 side"], axis=0)
-cg("Enginem75", ["m75 back", "m75 side"], axis=0)
-cg("Enginet34", ["t34 back", "t34 side"], axis=0)
-cg("EngineCrusader", ["Crusader back", "Crusader side"], axis=0)
-cg("EngineCromwell", ["Cromwell back", "Cromwell side"], axis=0)
-
-cg("EngineTiger", ["Tiger back", "Tiger side"], axis=0)
-cg("EnginemPanther", ["Panther back", "Panther side"], axis=0)
-cg("Enginetj76", ["j76 back", "j76 side"], axis=0)
-cg("Engineis1", ["is1 back", "is1 side"], axis=0)
-cg("EngineChurchill", ["Churchill back", "Churchill side"], axis=0)
-cg("EngineFirefly", ["Firefly back", "Firefly side"], axis=0)
 
 cg("EngineGerman", ["EngineLuchs", "EngineP4", "EngineTiger", "EnginemPanther", "white", "white"])
 cg("EngineAmerican", ["EngineStuart", "Enginem75", "Enginetj76", "white", "white", "white"])
 cg("EngineRussian", ["Enginet70", "Enginet34", "Engineis1", "white", "white", "white"])
 cg("EngineBritish", ["EngineHoney", "EngineTetrarch", "EngineCrusader", "EngineCromwell", "EngineChurchill", "EngineFirefly"])
-"""
+#"""
 
 
 cg("EngineLuchs", ["Luchs back", "Luchs side"], axis=0)
